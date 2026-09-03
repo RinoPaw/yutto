d6a8f04c70854dd5b32db2b6f72a6ad3ab6cec42
Rino <71957726+RinoPaw@users.noreply.github.com>
2026-09-03 23:42:41 +0800

    refactor: split fav and watch later parser

--- src/yutto/parser/ugc_list_parser.py
+++ src/yutto/parser/ugc_list_parser.py
@@ -5,30 +5,24 @@ from typing import TYPE_CHECKING
 from urllib.parse import parse_qs, urlparse
 
 from yutto.parser._abc import Parser
-from yutto.parser.parser import _BILIBILI, _SPACE_BILIBILI, _URL_END
-from yutto.source import UgcListSource
-from yutto.types import BilibiliId, FId
+from yutto.parser.parser import _SPACE_BILIBILI, _URL_END
+from yutto.source import UgcFavSource
+from yutto.types import FId
 
 if TYPE_CHECKING:
     from yutto.source import Source, SourceOptions
 
 
-class UgcListParser(Parser):
-    _WATCH_LATER_URL = re.compile(rf"{_BILIBILI}/(?:list/)?watchlater{_URL_END}", re.IGNORECASE)
+class UgcFavParser(Parser):
     _FAVOURITE_URL = re.compile(rf"{_SPACE_BILIBILI}/(?P<mid>[0-9]+)/favlist{_URL_END}", re.IGNORECASE)
 
     def parse(self, url: str, options: SourceOptions) -> Source | None:
         query = parse_qs(urlparse(url).query, keep_blank_values=True)
-
-        if self._WATCH_LATER_URL.fullmatch(url):
-            return UgcListSource(id=BilibiliId("watchlater"), kind="watch_later", options=options)
-
-        if self._FAVOURITE_URL.fullmatch(url):
-            if self._single_query_value(query, "ftype") == "collect":
-                return None
-            fid = self._single_query_value(query, "fid")
-            if fid is None:
-                return None
-            return UgcListSource(id=FId(fid), kind="favourite", options=options)
-
-        return None
+        if not self._FAVOURITE_URL.fullmatch(url):
+            return None
+        if self._single_query_value(query, "ftype") == "collect":
+            return None
+        fid = self._single_query_value(query, "fid")
+        if fid is None:
+            return None
+        return UgcFavSource(id=FId(fid), options=options)
