f8e8ea6425e225d8a483f0cea5febac8c1a66f95
Rino <71957726+RinoPaw@users.noreply.github.com>
2026-09-03 23:06:10 +0800

    ♻️ refactor: rename ugc list source to fav source

--- src/yutto/source/__init__.py
+++ src/yutto/source/__init__.py
@@ -6,7 +6,7 @@ from yutto.source.bangumi_season_source import BangumiSeasonSource
 from yutto.source.cheese_episode_source import CheeseEpisodeSource
 from yutto.source.cheese_season_source import CheeseSeasonSource
 from yutto.source.ugc_collection_source import UgcCollectionSource
-from yutto.source.ugc_list_source import UgcListSource
+from yutto.source.ugc_fav_source import UgcFavSource
 from yutto.source.ugc_series_source import UgcSeriesSource
 from yutto.source.ugc_video_source import UgcVideoSource
 
@@ -19,7 +19,7 @@ __all__ = [
     "Source",
     "SourceOptions",
     "UgcCollectionSource",
-    "UgcListSource",
+    "UgcFavSource",
     "UgcSeriesSource",
     "UgcVideoSource",
 ]
